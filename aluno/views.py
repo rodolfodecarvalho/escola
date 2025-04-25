dd098e4a050cba1868a650d08d3f8d573e003cba
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2025-04-25 09:30:39 -0300

    feat: configure link aluno in menu

--- aluno/views.py
+++ aluno/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def aluno(request):
+    return render(request, "aluno.html")
--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
-    'home'
+    'home',
+    'aluno',
 ]
 
 MIDDLEWARE = [
