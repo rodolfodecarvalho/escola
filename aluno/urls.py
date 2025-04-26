9aa1d0bc56299bda1b679cec714a63170c6e935c
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2025-04-25 21:31:36 -0300

    feat: add function cadastrar

--- aluno/urls.py
+++ aluno/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.aluno, name="aluno")
+    path('', views.aluno, name="aluno"),
+    path('cadastrar', views.cadastrar, name="cadastrar"),
 ]
--- aluno/views.py
+++ aluno/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def aluno(request):
     return render(request, "aluno.html")
+
+def cadastrar(request):
+    return render(request, "cadastrar.html")
