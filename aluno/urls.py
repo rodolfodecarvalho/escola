75d695e1c4fafc4c4ddbf30ba16faca08fb74214
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2025-04-29 22:11:02 -0300

    feat: update aluno

--- aluno/urls.py
+++ aluno/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.aluno, name="aluno"),
+    path("excluir/<int:id>", views.exluir, name="exluir"),
+    path("atualizar/<int:id>", views.atualizar, name="atualizar"),
     path('cadastro/', views.Cadastro.as_view(), name="cadastro")
 ]
--- aluno/views.py
+++ aluno/views.py
@@ -1,22 +1,47 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 
 from aluno.forms import AlunoForm
+from aluno.models import Aluno
 
 def aluno(request):
     return render(request, "aluno.html")
 
+def exluir(request, id):
+    aluno = get_object_or_404(Aluno, id=id)
+
+    aluno.delete()
+
+    return redirect("cadastro")
+
+def atualizar(request, id):
+    aluno = get_object_or_404(Aluno, id=id)
+
+
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    telefone = request.POST.get('telefone')
+
+    aluno.nome = nome
+    aluno.email = email
+    aluno.telefone = telefone
+
+    aluno.save()
+
+    return redirect('cadastro')
 
 class Cadastro(View):
     template_name = 'cadastro.html'
     form_class = AlunoForm
 
     def get(self, request):
-        return render(request, self.template_name, {'form' : self.form_class()})
+        alunos = Aluno.objects.all()
+
+        return render(request, self.template_name, {'form' : self.form_class(), 'alunos': alunos})
     
     def post(self, request):
         form = self.form_class(request.POST)
+        alunos = Aluno.objects.all()
 
         if form.is_valid():
             obj = form.save(commit=False)
@@ -24,6 +49,6 @@ class Cadastro(View):
 
             obj.save()
 
-            return HttpResponse('Cadastro realizado com sucesso')
+            return render(request, self.template_name, {'form' : self.form_class(), 'alunos': alunos})
         
-        return render(request, self.template_name, {'form' : form})
+        return render(request, self.template_name, {'form' : form, 'alunos': alunos})
